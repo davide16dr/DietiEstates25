a2a21e523bbde2b368802a915f4a856b8693eafb
davide16dr <davideranavolo16@gmail.com>
2025-12-18 12:20:06 +0100

    Created Navbar and Footer component and added logo

---
+++ Frontend-DietiEstates25/src/app/shared/components/navbar.component/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  @Input() showAuthActions = true;
+
+  @Input() brandText = 'DietiEstates';
+  @Input() brandSuffix = '25';
+}
