8ca55a2f7932efcf9adcc86371e756218dba86e8
davide16dr <davideranavolo16@gmail.com>
2025-12-21 13:32:10 +0100

    Load new Google Maps API key from environment configuration

--- Frontend-DietiEstates25/src/app/app.config.ts
+++ Frontend-DietiEstates25/src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&v=weekly`;
+script.defer = true;
+document.head.appendChild(script);
 
 export const appConfig: ApplicationConfig = {
   providers: [
